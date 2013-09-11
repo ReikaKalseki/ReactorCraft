de67333f87f92e23339a05b6cc84a7e71f06e7d7
Reika <reikasminecraft@gmail.com>
2013-09-11 19:13:01 -0400

    Major bugfix

--- Registry/ReactorOptions.java
+++ Registry/ReactorOptions.java
@@ -13,7 +13,6 @@ import net.minecraftforge.common.Configuration;
 import Reika.DragonAPI.Exception.RegistrationException;
 import Reika.DragonAPI.Interfaces.ConfigList;
 import Reika.ReactorCraft.ReactorCraft;
-import Reika.RotaryCraft.RotaryCraft;
 
 public enum ReactorOptions implements ConfigList {
 
@@ -56,12 +55,12 @@ public enum ReactorOptions implements ConfigList {
 
 	public float setDecimal(Configuration config) {
 		if (!this.isDecimal())
-			throw new RegistrationException(RotaryCraft.instance, "Config Property \""+this.getLabel()+"\" is not decimal!");
+			throw new RegistrationException(ReactorCraft.instance, "Config Property \""+this.getLabel()+"\" is not decimal!");
 		return (float)config.get("Control Setup", this.getLabel(), defaultFloat).getDouble(defaultFloat);
 	}
 
 	public float getFloat() {
-		return (Float)RotaryCraft.config.getControl(this.ordinal());
+		return (Float)ReactorCraft.config.getControl(this.ordinal());
 	}
 
 	public Class getPropertyType() {
