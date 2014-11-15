3105f293c69b379defc03f249537129c1b7944bf
Reika <reikasminecraft@gmail.com>
2014-11-15 14:29:39 -0500

    Update

--- TileEntities/Fusion/TileEntityToroidMagnet.java
+++ TileEntities/Fusion/TileEntityToroidMagnet.java
@@ -73,7 +73,7 @@ public class TileEntityToroidMagnet extends TileEntityReactorBase implements Scr
 			if (this.canAffect(e)) {
 				e.setTarget(tg[0], tg[2]);
 				e.magnetOrdinal = this.getOrdinal();
-				tank.removeLiquid(25);
+				tank.removeLiquid(10);
 			}
 			else {
 				ReactorAchievements.ESCAPE.triggerAchievement(this.getPlacer());
